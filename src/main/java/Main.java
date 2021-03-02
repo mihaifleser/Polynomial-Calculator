427bf098bab12a363bdd5729a3f324a7b2e51c44
Mihai Fleser <mihaifleser@yahoo.com>
2021-03-02 12:44:17 +0200

    Update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,19 +6,26 @@ public class Main {
         PolyHandler ph = new PolyHandler();
         Polynomial p1 = new Polynomial("3x^1 + 2x^2 - 7x^4");
         System.out.println("P1: " + p1.writePoly());
-
         Polynomial p2 = new Polynomial("-4x^4 + 9321x^-5 + 32x^0 + 5x^2 +5x^2");
         System.out.println("P2: " + p2.writePoly());
-        //Polynomial result1;
-        //result1 = PolyHandler.addPolynomials(p1,p2);
-        //System.out.println(result1.writePoly());
+
+        Polynomial result1;
+        result1 = ph.addPolynomials(p1,p2);
+        System.out.println(result1.writePoly());
+        System.out.println("///////////////");
+        System.out.println("P1: " + p1.writePoly());
+        System.out.println("P2: " + p2.writePoly());
+        System.out.println("///////////////");
+
+
         Polynomial result2;
-        result2 = ph.addPolynomials(p1,p2);
+        result2 = ph.substractPolynomials(p1,p2);
         System.out.println(result2.writePoly());
 
         System.out.println("///////////////");
         System.out.println("P1: " + p1.writePoly());
         System.out.println("P2: " + p2.writePoly());
+        System.out.println("///////////////");
 
     }
 }
--- src/main/java/PolyHandler.java
+++ src/main/java/PolyHandler.java
@@ -20,24 +20,25 @@ public class PolyHandler {
         {
             result.addMonomial(m2);
         }
-
-
-
         return result;
     }
     public Polynomial substractPolynomials(Polynomial p1, Polynomial p2)
     {
+        Polynomial aux1 = new Polynomial(p1);
+        Polynomial aux2 = new Polynomial();
         Polynomial result = new Polynomial();
-        for(Monomial m:p1.getContent())
-        {
-            result.addMonomial(m);
-        }
         for(Monomial m:p2.getContent())
         {
-            Monomial aux = new Monomial(m.getCoefficient(), m.getDegree());
-            aux.setCoefficient(aux.getCoefficient() * -1);
-            System.out.println("+++++ " + aux.writeMonomial());
-            result.addMonomial(aux);
+            Float auxCoeff = m.getCoefficient() * (-1);
+            aux2.addMonomial(new Monomial(auxCoeff,m.getDegree()));
+        }
+        for(Monomial m1:aux1.getContent())
+        {
+            result.addMonomial(m1);
+        }
+        for(Monomial m2:aux2.getContent())
+        {
+            result.addMonomial(m2);
         }
         return  result;
     }
